9f198de9807330aafff4a5df2dc1ba34bcba5665
BBomers <bob.Bomers@student.graafschapcollege.nl>
2025-04-23 11:55:08 +0200

    DBContext create

---
+++ CovautoAPI.Domain/Klasse/CovautoContext.cs
@@ -0,0 +1,19 @@
+﻿using CovautoAPI.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovautoAPI.Domain.Klasse
+{
+    public class CovautoContext : DbContext
+    {
+
+        public CovautoContext(DbContextOptions<CovautoContext> options) : base(options) { }
+
+        public DbSet<LeenAuto> leenAuto { get; set; }
+        public DbSet<Reserveringen> reserveringen { get; set; }
+    }
+}
