8e3feb93b1c94fd7889f9c93933c8b5282919ab1
BBomers <bob.Bomers@student.graafschapcollege.nl>
2025-05-07 12:00:38 +0200

    mvc add

--- CovautoAPI.Domain/Entities/Reserveringen.cs
+++ CovautoAPI.Domain/Entities/Reserveringen.cs
@@ -10,10 +10,7 @@ namespace CovautoAPI.Domain.Entities
     {
         public int Id { get; set; }
         public int LeenAutoID { get; set; }
-        public string Postcode { get; set; }
-        public string Straat { get; set; }
-        public string Stad { get; set; }
-        public DateTime ReserveerDatum { get; set; }
+        public DateTime ReserveerDatum { get; set; } // Tijd van platsing van reservering
         public DateTime BoekDatumVanaf { get; set; }
         public DateTime BoekDatumTot { get; set; }
     }
