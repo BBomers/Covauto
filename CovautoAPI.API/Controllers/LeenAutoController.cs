cafe4c2095ea12f9ec11aff391c79425ad74bbc7
honzi <honzi.hermanek@student.graafschapcollege.nl>
2025-05-09 11:05:55 +0200

    ghgdf

---
+++ CovautoAPI.API/Controllers/LeenAutoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CovautoAPI.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LeenAutoController : Controller
+    {
+        public readonly LeenAutoRepository leenAutoRepository;
+
+        public LeenAutoController(LeenAutoRepository leenAutoRepository)
+        {
+            this.leenAutoRepository = leenAutoRepository;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<FullLeenAutoListItem>> GetAllLeenAutos() 
+        {
+            var leenAutos = leenAutoRepository.getAllLeenAutos();
+            return Ok(leenAutos);
+        }
+
+        [HttpGet("search/{kenteken}")]
+        public ActionResult<IEnumerable<FullLeenAutoListItem>> ZoekLeenAutos([FromQuery] string )
+    }
+}
