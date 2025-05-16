cbdb2dca4c9692c2bb2fdd7500f3388d0ebdea4e
honzi <honzi.hermanek@student.graafschapcollege.nl>
2025-05-16 11:50:42 +0200

    Added ReserveringController and ReserveringDataController

---
+++ CovautoAPI.API/Controllers/ReserveringController.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CovautoAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReserveringController : ControllerBase
+    {
+        private readonly IReserveringRepository reserveringRepository;
+
+        public ReserveringController(IReserveringRepository reserveringRepository)
+        {
+            this.reserveringRepository = reserveringRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GeefReservaties()
+        {
+            return Ok(await reserveringRepository.GeefAlleReservatiesAsync());
+        }
+
+        [HttpGet("search/{titel}")]
+        public async Task<IActionResult> ZoekReservaties(string kenteken)
+        {
+            return Ok(await reserveringRepository.ZoekReserveringAsync(kenteken));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GeefReservatie(int id)
+        {
+            FullReservatieData? retVal = await reserveringRepository.GeefReservatieAsync(id);
+            return retVal != null ? Ok(retVal) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MaakBoek(MaakReservatie reservatie)
+        {
+            try
+            {
+                var id = await reserveringRepository.CreateReservatieAsync(reservatie);
+
+                return Ok(id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateReservatie(int id, UpdateReservatie reservatie)
+        {
+            try
+            {
+                await reserveringRepository.UpdateBoekAsync(id, reservatie);
+                return Ok();
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteReservatie(int id)
+        {
+            try
+            {
+                await reserveringRepository.DeleteReservatieAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ CovautoAPI.API/Controllers/ReserveringDataController.cs
@@ -0,0 +1,83 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CovautoAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReserveringDataController : ControllerBase
+    {
+        private readonly IReserveringDataRepository reserveringDataRepository;
+
+        public ReserveringDataController(IReserveringDataRepository reserveringDataRepository)
+        {
+            this.reserveringDataRepository = reserveringDataRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GeefReservatieData()
+        {
+            return Ok(await reserveringDataRepository.GeefAlleReservatieDataAsync());
+        }
+
+        [HttpGet("search/{titel}")]
+        public async Task<IActionResult> ZoekReservatieData(string kenteken)
+        {
+            return Ok(await reserveringDataRepository.ZoekReservatieDataAsync(kenteken));
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GeefReservatie(int id)
+        {
+            FullReservatie? retVal = await reserveringDataRepository.GeefBoekAsync(id);
+            return retVal != null ? Ok(retVal) : NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> MaakBoek(MaakReservatieData reservatieData)
+        {
+            try
+            {
+                var id = await reserveringDataRepository.CreateReservatieAsync(reservatieData);
+
+                return Ok(id);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateReservatieData(int id, UpdateReservatieData reservatieData)
+        {
+            try
+            {
+                await reserveringDataRepository.UpdateBoekAsync(id, reservatie);
+                return Ok();
+            }
+            catch (ValidationException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("id")]
+        public async Task<IActionResult> DeleteReservatieData(int id)
+        {
+            try
+            {
+                await reserveringDataRepository.DeleteReservatieAsync(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
