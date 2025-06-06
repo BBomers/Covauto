1b208bafb75a2d8cdd124734bc23c0eeeec9c13e
BBomers <bob.Bomers@student.graafschapcollege.nl>
2025-06-06 10:58:54 +0200

    working HTTPClient

--- Covauto.blazor/Program.cs
+++ Covauto.blazor/Program.cs
@@ -6,9 +6,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 var apiUrl = "https://localhost:7119/";
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUrl) });
+
+
 
 await builder.Build().RunAsync();
